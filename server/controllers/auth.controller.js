257f56bed2b8e36edfbaf975231e2162e60a08c1
vklata <151921976+vklata@users.noreply.github.com>
2026-03-09 02:44:03 +0530

    Refactor googleAuth and logOut functions

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,30 +1,43 @@
 import User from "../models/user.model.js"
 import jwt from "jsonwebtoken"
-export const googleAuth=async (req,res)=>{
+export const googleAuth = async (req,res)=>{
 try {
-    const {name,email,avatar}=req.body
-    if(!email){
-        return res.status(400).json({
-            message:"email is required"
-        })
-    }
-    const user=await User.findOne({email})
-    if(!user){
-      user=await User.create({name,email,avatar})
-    }
-    const token=await jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"7d"})
 
-    res.cookie("token",token,{
-        httpOnly:true,
-        secure:true,
-        sameSite:"none",
-        maxAge:7*24*60*60*1000
-    })
+const { name,email,avatar } = req.body
 
-    return res.status(200).json(user)
-} catch (error) {
-    
-    return res.status(500).json({message:`google auth error ${error}`})
+if(!email){
+return res.status(400).json({
+message:"email is required"
+})
+}
+
+let user = await User.findOne({email})
+
+if(!user){
+user = await User.create({name,email,avatar})
+}
+
+const token = jwt.sign(
+{ id:user._id },
+process.env.JWT_SECRET,
+{ expiresIn:"7d" }
+)
+
+res.cookie("token",token,{
+httpOnly:true,
+secure:true,
+sameSite:"none",
+maxAge:7*24*60*60*1000
+})
+
+return res.status(200).json(user)
+
+}catch(error){
+
+return res.status(500).json({
+message:`google auth error ${error}`
+})
+
 }
 }
 
@@ -43,3 +56,4 @@ try {
 }
 
 }
+
