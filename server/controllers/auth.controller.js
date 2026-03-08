f34fcf4d5531ca635ce43bfae4e4c19cc24253ff
vklata <151921976+vklata@users.noreply.github.com>
2026-03-09 01:00:23 +0530

    Update cookie settings for authentication methods

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -16,8 +16,8 @@ try {
 
     res.cookie("token",token,{
         httpOnly:true,
-        secure:false,
-        sameSite:"strict",
+        secure:true,
+        sameSite:"none",
         maxAge:7*24*60*60*1000
     })
 
@@ -33,12 +33,13 @@ export const logOut=async (req,res)=>{
 try {
      res.clearCookie("token",{
         httpOnly:true,
-        secure:false,
-        sameSite:"strict"
+        secure:true,
+        sameSite:"none"
     })
 
     return res.status(200).json({message :"log out successfully"})
 } catch (error) {
     return res.status(500).json({message:`log out error ${error}`})
 }
+
 }
